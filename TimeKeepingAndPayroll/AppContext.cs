f128726956d8b5f7336612eb1cafacdeec9ec10f
Kelvin Lee <kahokelvinlee@gmail.com>
2020-04-03 18:48:41 -0700

    edit: AppContext createdb if not exist

--- TimeKeepingAndPayroll/AppContext.cs
+++ TimeKeepingAndPayroll/AppContext.cs
@@ -25,7 +25,7 @@ namespace TimeKeepingAndPayroll
         public DbSet<Service> Service { get; set; }
         public DbSet<Shift> Shift { get; set; }
     }
-    public class AppDBInitializer : DropCreateDatabaseIfModelChanges<AppContext>
+    public class AppDBInitializer : CreateDatabaseIfNotExists<AppContext>
     {
         protected override void Seed(AppContext context)
         {
