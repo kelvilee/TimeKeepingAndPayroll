8c85d023b2850c2d80894d227a17ed9e6c3bbbf1
Kelvin Lee <kahokelvinlee@gmail.com>
2020-04-10 00:09:23 -0700

    add: AttendanceTests

--- TimeKeepingAndPayroll/Tests/AttendanceTests.cs
+++ TimeKeepingAndPayroll/Tests/AttendanceTests.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using OpenQA.Selenium.Support.UI;
 using System;
 using System.IO;
+using System.Globalization;
 
 namespace TimeKeepingAndPayroll.Tests
 {
@@ -19,78 +20,46 @@ namespace TimeKeepingAndPayroll.Tests
         [SetUp]
         public void startBrowser()
         {
-            driver = new ChromeDriver(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..\\Tests"));
+            driver = new ChromeDriver();
             driver.Url = "http://localhost:51729/"; // you will need to run IIS Express on another instance of VS and run these tests on this one, make sure this matches your localhost:PORT# of the running instance of IIS Express
         }
 
         [Test]
-        public void createCustomer()
+        public void managerFixTime()
         {
-            //IWebElement customerNavBtn = driver.FindElement(By.XPath(".//*[@class='navbar navbar-inverse navbar-fixed-top']//div/div[2]/ul/li[2]/a"));
-            //customerNavBtn.Click();
-            //IWebElement createCustomer = driver.FindElement(By.XPath(".//*[@class='container body-content']//p/a"));
-            //createCustomer.Click();
-            //IWebElement fNameTextBox = driver.FindElement(By.XPath(".//*[@id='FirstName']"));
-            //IWebElement createBtn = driver.FindElement(By.XPath(".//*[@class='btn btn-default']"));
-            //fNameTextBox.SendKeys("Jake");
-            //createBtn.Click();
+            IWebElement employeeNav = driver.FindElement(By.XPath("//a[@href='/Employees']"));
+            employeeNav.Click();
+            IWebElement getEmployees = driver.FindElement(By.XPath(".//*[@class='table']//tbody/tr[2]/td[11]/a[4]"));
+            getEmployees.Click();
+            IWebElement editAttendance = driver.FindElement(By.XPath(".//*[@class='table']//tbody/tr[2]/td[7]/a[1]"));
+            editAttendance.Click();
+            IWebElement createAttendance = driver.FindElement(By.XPath("//a[@href='/Attendances/Create']"));
+            createAttendance.Click();
+            IWebElement eid = driver.FindElement(By.XPath(".//*[@id='EmployeeID']"));
+            IWebElement time = driver.FindElement(By.XPath(".//*[@id='Timestamp']"));
+            IWebElement act = driver.FindElement(By.XPath(".//*[@id='Activity']"));
+            var eidSelect = new SelectElement(eid);
+            eidSelect.SelectByValue("2222");
+            time.SendKeys("04102020");
+            time.SendKeys("0545PM");
+            var ActivitySelect = new SelectElement(act);
+            ActivitySelect.SelectByIndex(2);
+            IWebElement createBtn = driver.FindElement(By.XPath(".//*[@class='btn btn-default']"));
+            createBtn.Click();
         }
 
-        //[Test]
-        //public void createCustomerContact()
-        //{
-        //    IWebElement customerNavBtn = driver.FindElement(By.XPath(".//*[@class='navbar navbar-inverse navbar-fixed-top']//div/div[2]/ul/li[2]/a"));
-        //    customerNavBtn.Click();
-        //    IWebElement createCustomerContact = driver.FindElement(By.XPath(".//*[@class='table']//tbody/tr[2]/td[3]/a[4]"));
-        //    createCustomerContact.Click();
-        //    IWebElement createBtn = driver.FindElement(By.XPath(".//*[@class='container body-content']//p/a"));
-        //    createBtn.Click();
-        //    IWebElement fNameTextBox = driver.FindElement(By.XPath(".//*[@id='FirstName']"));
-        //    IWebElement insertBtn = driver.FindElement(By.XPath(".//*[@class='btn btn-default']"));
-        //    IWebElement customer = driver.FindElement(By.XPath(".//*[@id='CustomerID']"));
-        //    var selectTest = new SelectElement(customer);
-        //    // Select a value from the dropdown				
-        //    selectTest.SelectByValue("0b0f84bf-a1d2-42c1-8519-d3ab046125dc");
-        //    fNameTextBox.SendKeys("Jake");
-        //    insertBtn.Click();
-        //}
-
-        //[Test]
-        //public void delCustomerContact()
-        //{
-        //    IWebElement customerNavBtn = driver.FindElement(By.XPath(".//*[@class='navbar navbar-inverse navbar-fixed-top']//div/div[2]/ul/li[2]/a"));
-        //    customerNavBtn.Click();
-        //    IWebElement openCustomerContact = driver.FindElement(By.XPath(".//*[@class='table']//tbody/tr[2]/td[3]/a[4]"));
-        //    openCustomerContact.Click();
-        //    IWebElement delCustomer = driver.FindElement(By.XPath(".//*[@class='table']//tbody/tr[2]/td[4]/a[1]"));
-        //    delCustomer.Click();
-        //    IWebElement delBtn = driver.FindElement(By.XPath(".//*[@class='btn btn-default']"));
-        //    delBtn.Click();
-        //}
-
-        //[Test]
-        //public void updateCustomer()
-        //{
-        //    IWebElement customerNavBtn = driver.FindElement(By.XPath(".//*[@class='navbar navbar-inverse navbar-fixed-top']//div/div[2]/ul/li[2]/a"));
-        //    customerNavBtn.Click();
-        //    IWebElement editCustomer = driver.FindElement(By.XPath(".//*[@class='table']//tbody/tr[2]/td[3]/a[1]"));
-        //    editCustomer.Click();
-        //    IWebElement descTextBox = driver.FindElement(By.XPath(".//*[@id='Description']"));
-        //    IWebElement saveBtn = driver.FindElement(By.XPath(".//*[@class='btn btn-default']"));
-        //    descTextBox.SendKeys("yare yare darze");
-        //    saveBtn.Click();
-        //}
-
-        //[Test]
-        //public void deleteCustomer()
-        //{
-        //    IWebElement customerNavBtn = driver.FindElement(By.XPath(".//*[@class='navbar navbar-inverse navbar-fixed-top']//div/div[2]/ul/li[2]/a"));
-        //    customerNavBtn.Click();
-        //    IWebElement delCustomer = driver.FindElement(By.XPath(".//*[@class='table']//tbody/tr[2]/td[3]/a[3]"));
-        //    delCustomer.Click();
-        //    IWebElement delBtn = driver.FindElement(By.XPath(".//*[@class='btn btn-default']"));
-        //    delBtn.Click();
-        //}
+        [Test]
+        public void employeePunchIn()
+        {
+            IWebElement loginNav = driver.FindElement(By.XPath("//a[@href='/Employees/Login']"));
+            loginNav.Click();
+            IWebElement eid = driver.FindElement(By.XPath(".//*[@id='EmployeeID']"));
+            IWebElement pass = driver.FindElement(By.XPath(".//*[@id='Password']"));
+            eid.SendKeys("2222");
+            pass.SendKeys("123");
+            IWebElement loginBtn = driver.FindElement(By.XPath(".//*[@class='btn btn-default']"));
+            loginBtn.Click();
+        }
 
         [TearDown]
         public void closeBrowser()
